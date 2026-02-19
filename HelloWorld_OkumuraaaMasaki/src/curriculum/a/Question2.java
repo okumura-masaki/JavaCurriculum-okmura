bd2199bfb9a9342fa649def21dc8dae9089f52a5
PC User <PC User@192.168.1.5>
2026-02-19 18:14:27 +0900

    課題変数①(2)

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question2.java
@@ -0,0 +1,20 @@
+package curriculum.a;
+
+public class Question2 {
+    public static void main(String[] args) {
+
+        byte byteNum = 10;
+        short shortNum = 100;
+        int intNum = 1000;
+        long longNum = 10000L;
+
+        float floatNum = 9.5f;
+        double doubleNum = 10.5;
+
+        char letter = 'a';
+        String letters = "ハロー";
+
+        boolean isBoolean = true;
+    }
+}
+
