7365d1d85fbe17e0668291f0d209f0bcad5e1a40
PC User <PC User@192.168.1.5>
2026-02-19 15:26:58 +0900

    課題変数①

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question1.java
@@ -0,0 +1,30 @@
+package curriculum.a;
+
+public class Question1 {
+
+    public static void main(String[] args) {
+
+        byte byteNum = 10;
+        short shortNum = 100;
+        int intNum = 1000;
+        long longNum = 10000L;
+
+        float floatNum = 3.14f;
+        double doubleNum = 3.14159;
+
+        char letter = 'A';
+        String letters = "Hello";
+
+        boolean isBoolean = true;
+
+        System.out.println(byteNum);
+        System.out.println(shortNum);
+        System.out.println(intNum);
+        System.out.println(longNum);
+        System.out.println(floatNum);
+        System.out.println(doubleNum);
+        System.out.println(letter);
+        System.out.println(letters);
+        System.out.println(isBoolean);
+    }
+}
