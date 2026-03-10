2f1058178e816caa00618c25bfe67a7750251fce
PC User <PC User@192.168.1.5>
2026-03-10 21:03:01 +0900

    変数②Question8

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question8.java
@@ -0,0 +1,17 @@
+package curriculum.a;
+
+public class Question8 {
+
+    public static void main(String[] args) {
+
+        // double型の変数 price を宣言して 99.99 を代入
+        double price = 99.99;
+
+        // price を int型に変換
+        int intPrice = (int) price;
+
+        // 結果を表示
+        System.out.println("整数価格: " + intPrice);
+
+    }
+}
