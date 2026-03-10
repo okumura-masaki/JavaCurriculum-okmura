de87f09749239fe92acf2b1b6cc5fc01fab5f051
PC User <PC User@192.168.1.5>
2026-03-10 20:32:22 +0900

    変数②Question4

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question4.java
@@ -0,0 +1,17 @@
+package curriculum.a;
+
+public class Question4 {
+
+    public static void main(String[] args) {
+
+        // String型の変数nameを宣言
+        String name;
+
+        // nameに値を代入
+        name = "山田太郎";
+
+        // nameを使って出力
+        System.out.println("こんにちは、" + name + "さん！");
+
+    }
+}
