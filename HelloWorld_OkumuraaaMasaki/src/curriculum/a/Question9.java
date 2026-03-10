27f05883b18a55d95e601649e32f5f576abcee03
PC User <PC User@192.168.1.5>
2026-03-10 21:05:20 +0900

    変数②Question9

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question9.java
@@ -0,0 +1,17 @@
+package curriculum.a;
+
+public class Question9 {
+
+    public static void main(String[] args) {
+
+        // String型の変数 numStr に "123" を代入
+        String numStr = "123";
+
+        // String を int に変換
+        int num = Integer.parseInt(numStr);
+
+        // 10 を足して表示
+        System.out.println("変換後の値: " + (num + 10));
+
+    }
+}
