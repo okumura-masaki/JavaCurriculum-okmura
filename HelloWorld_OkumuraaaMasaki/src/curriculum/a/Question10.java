86d03d554f4c3d4122391c29f9d9ccaa601d065d
PC User <PC User@192.168.1.5>
2026-03-10 21:45:35 +0900

    変数②Question10

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question10.java
@@ -0,0 +1,17 @@
+package curriculum.a;
+
+public class Question10 {
+
+    public static void main(String[] args) {
+
+        // int型の変数 num に 50 を代入
+        int num = 50;
+
+        // num を String型に変換
+        String numStr = String.valueOf(num);
+
+        // 表示
+        System.out.println("得点: " + numStr + "点");
+
+    }
+}
