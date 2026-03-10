2753cc12c29226364f450d9420cd4b960ef13a4e
PC User <PC User@192.168.1.5>
2026-03-10 20:08:20 +0900

    変数②Question3.java

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question3.java
@@ -0,0 +1,40 @@
+package curriculum.a;
+
+public class Question3 {
+
+    public static void main(String[] args) {
+
+        // 11110 11110を出力
+        int num1 = 11110;
+        System.out.println(num1 + " " + num1);
+
+        // 20 20を出力
+        int num2 = 20;
+        System.out.println(num2 + " " + num2);
+
+        // a ハロー true a ハロー trueを出力
+        String a = "a";
+        String hello = "ハロー";
+        boolean flag = true;
+        System.out.println(a + " " + hello + " " + flag + " " + a + " " + hello + " " + flag);
+
+        // 11130 数字を全て足して出力
+        int number = 11130;
+        int sum = 1 + 1 + 1 + 3 + 0;
+        System.out.println(sum);
+
+        // 10000000000 小数点以外の数字を全てかけて出力
+        long bigNumber = 10000000000L;
+        long product = 1 * 0 * 0 * 0 * 0 * 0 * 0 * 0 * 0 * 0 * 0;
+        System.out.println(product);
+
+        // 0.105 10.5割る100をして出力
+        double x = 10.5;
+        System.out.println(x / 100);
+
+        // -90 10引く100をして出力
+        int y = 10;
+        System.out.println(y - 100);
+
+    }
+}
