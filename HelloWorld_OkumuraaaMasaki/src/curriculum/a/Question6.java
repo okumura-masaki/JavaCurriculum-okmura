0744602109346518ba1bac6bb31ad5148df17754
PC User <PC User@192.168.1.5>
2026-03-10 20:39:06 +0900

    変数②Question6

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question6.java
@@ -0,0 +1,20 @@
+package curriculum.a;
+
+public class Question6 {
+
+    public static void main(String[] args) {
+
+        // num1 を宣言して10を代入
+        int num1 = 10;
+
+        // num2 を宣言して5を代入
+        int num2 = 5;
+
+        // num1とnum2を足してsumに代入
+        int sum = num1 + num2;
+
+        // 結果を表示
+        System.out.println(sum);
+
+    }
+}
