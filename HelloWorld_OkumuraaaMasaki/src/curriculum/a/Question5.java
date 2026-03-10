8d7fbfebe3fab94b7a45faf1c19fd64c012e94be
PC User <PC User@192.168.1.5>
2026-03-10 20:34:50 +0900

    変数②Question5

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question5.java
@@ -0,0 +1,17 @@
+package curriculum.a;
+
+public class Question5 {
+
+    public static void main(String[] args) {
+
+        // int型の変数ageを宣言
+        int age;
+
+        // ageに25を代入
+        age = 25;
+
+        // ageを使って出力
+        System.out.println("年齢: " + age + "歳");
+
+    }
+}
