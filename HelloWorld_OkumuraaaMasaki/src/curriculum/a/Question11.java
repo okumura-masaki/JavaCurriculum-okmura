7539004f959dd92321cae032f87e70c545144fa3
PC User <PC User@192.168.1.5>
2026-03-10 22:12:31 +0900

    変数②Question11

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question11.java
@@ -0,0 +1,20 @@
+package curriculum.a;
+
+public class Question11 {
+
+    public static void main(String[] args) {
+
+        // int型の変数aに10を代入
+        int a = 10;
+
+        // int型の変数bに20を代入
+        int b = 20;
+
+        // 条件演算子を使って結果をbooleanに代入
+        boolean result = (a < b) ? true : false;
+
+        // resultを表示
+        System.out.println(result);
+
+    }
+}
