5ffb33395a07b17c779e195a0d1269e70113a4fa
PC User <PC User@192.168.1.5>
2026-03-10 20:46:25 +0900

    変数②Question7

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question7.java
@@ -0,0 +1,17 @@
+package curriculum.a;
+
+public class Question7 {
+
+    public static void main(String[] args) {
+
+        // score を宣言して80を代入
+        int score = 80;
+
+        // score に20を加えて更新
+        score = score + 20;
+
+        // 結果を表示
+        System.out.println("最終スコア: " + score);
+
+    }
+}
