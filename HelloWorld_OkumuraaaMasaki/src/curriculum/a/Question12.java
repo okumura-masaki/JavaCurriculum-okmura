6656fed2d3e243ee8f82fd19fd31f3e7f1194d10
PC User <PC User@192.168.1.5>
2026-03-19 20:45:53 +0900

    Question12

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question12.java
@@ -0,0 +1,11 @@
+package curriculum.a;
+
+public class Question12 {
+    public static void main(String[] args) {
+        int x = 15;
+
+        String result = (x >= 10) ? "OK" : "NG";
+
+        System.out.println(result);
+    }
+}
