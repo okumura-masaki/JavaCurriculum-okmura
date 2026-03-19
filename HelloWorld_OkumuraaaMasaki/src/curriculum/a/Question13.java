1eea8ffe18177410dbde6bd039ee86a13f2235a5
PC User <PC User@192.168.1.5>
2026-03-19 20:47:17 +0900

    Question13

---
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/a/Question13.java
@@ -0,0 +1,11 @@
+package curriculum.a;
+
+public class Question13 {
+    public static void main(String[] args) {
+        String text = "私はJavaが好きです。Javaは楽しい！";
+
+        String result = text.replace("Java", "Python");
+
+        System.out.println(result);
+    }
+}
