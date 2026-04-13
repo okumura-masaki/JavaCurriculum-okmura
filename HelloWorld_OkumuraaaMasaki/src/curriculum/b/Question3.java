3c815b1a10436df1f6c0adcea039be22ff82490c
PC User <PC User@192.168.1.5>
2026-04-13 23:06:45 +0900

    課題 while型反復処理

--- HelloWorld_OkumuraaaMasaki/src/curriculum/b/Question3.java
+++ HelloWorld_OkumuraaaMasaki/src/curriculum/b/Question3.java
@@ -59,7 +59,34 @@ public class Question3 {
             i8--;
         }
 
+        // Q9
+        int i9 = 1;
+        int sum9 = 0;
+        while (i9 <= 100) {
+            sum9 += i9;
+            i9++;
+        }
+        System.out.println("合計は：" + sum9);
+
+        // Q10
+        int num = -1;
+        while (num != 0) {
+            System.out.print("数値を入力してください（0で終了）：");
+            num = scanner.nextInt();
+        }
+        System.out.println("終了しました");
+
+        // Q11
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%02d * %02d = %02d", i, j, i * j);
+                if (j != 9) {
+                    System.out.print(" || ");
+                }
+            }
+            System.out.println();
+        }
+
         scanner.close();
     }
 }
-    
